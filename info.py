4bcd209c12ded801f0db2fafc557c79e6ad6d9b1
Joni <152844226+jonidm02@users.noreply.github.com>
2024-06-15 09:03:06 -0700

    Update info.py

--- info.py
+++ info.py
@@ -113,7 +113,7 @@ OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'sampleupi@upi')
 
 # for stream
 IS_STREAM = is_enabled('IS_STREAM', True)
-BIN_CHANNEL = environ.get("BIN_CHANNEL", "-1002226476471")
+BIN_CHANNEL = environ.get("BIN_CHANNEL", "-1002160683630")
 if len(BIN_CHANNEL) == 0:
     print('Error - BIN_CHANNEL is missing, exiting now')
     exit()
